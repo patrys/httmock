1a8dac1e4a312162ab103be359aef8fc993fd514
Patryk Zawadzki <patrys@room-303.com>
2013-04-05 17:08:44 +0200

    Add license, cosmetics

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 import os
 
-setup(name='httmock',
-      version='1.0',
-      description='A mocking library for requests.',
-      author='Patryk Zawadzki',
-      author_email='patrys@room-303.com',
-      url='https://github.com/patrys/httmock',
-      py_modules=['httmock'],
-      keywords=['requests', 'testing', 'mock'],
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Programming Language :: Python',
-          'Intended Audience :: Developers',
-          'Topic :: Software Development :: Testing',
-          'Operating System :: OS Independent',
-      ],
-      install_requires=['requests'],
-      long_description=open(
-          os.path.join(os.path.dirname(__file__), 'README.md'),
-      ).read().strip(),
-)
+LICENSE = open(
+    os.path.join(os.path.dirname(__file__), 'LICENSE')).read().strip()
+
+DESCRIPTION = open(
+    os.path.join(os.path.dirname(__file__), 'README.md')).read().strip()
+
+setup(
+    name='httmock',
+    version='1.0.1',
+    description='A mocking library for requests.',
+    author='Patryk Zawadzki',
+    author_email='patrys@room-303.com',
+    url='https://github.com/patrys/httmock',
+    py_modules=['httmock'],
+    keywords=['requests', 'testing', 'mock'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Operating System :: OS Independent'],
+    install_requires=['requests'],
+    license=LICENSE,
+    long_description=DESCRIPTION)
