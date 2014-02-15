81a523bf199991738ae8eca903a178601aa48082
Simon Weber <simon@simonmweber.com>
2014-02-14 22:54:34 -0500

    remove external calls in tests

--- tests.py
+++ tests.py
@@ -198,26 +198,18 @@ class ResponseTest(unittest.TestCase):
             response = requests.get('http://foo_bar')
             self.assertEqual(self.content, response.json())
 
-    def test_redirect_to_mock(self):
-        """Hit a real page that redirects to a mock."""
-
-        with HTTMock(google_mock):
-            response = requests.get('http://httpbin.org/redirect-to?url=http://google.com/')
-            self.assertEqual(len(response.history), 1)
-            self.assertEqual(response.content, 'Hello from Google')
-
     def test_mock_redirect(self):
         """Mock out a redirect."""
 
         @urlmatch(netloc='example.com')
         def get_mock(url, request):
             return {'status_code': 302,
-                    'headers': {'Location': 'http://httpbin.org/get'}}
+                    'headers': {'Location': 'http://google.com/'}}
 
-        with HTTMock(get_mock):
-            response = requests.get('http://example.com')
+        with HTTMock(get_mock, google_mock):
+            response = requests.get('http://example.com/')
             self.assertEqual(len(response.history), 1)
-            self.assertEqual(response.json()['url'], 'http://httpbin.org/get')
+            self.assertEqual(response.content, 'Hello from Google')
 
 
 suite = unittest.TestSuite()
