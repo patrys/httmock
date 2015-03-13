1201cc0488aeb11c756f14581e95cbcae38f3937
Patryk Zawadzki <patrys@room-303.com>
2015-03-13 18:38:26 +0100

    1.2.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ DESCRIPTION = open(
 
 setup(
     name='httmock',
-    version='1.2.2',
+    version='1.2.3',
     description='A mocking library for requests.',
     author='Patryk Zawadzki',
     author_email='patrys@room-303.com',
